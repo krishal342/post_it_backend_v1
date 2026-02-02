02f84447d7143d823c1e07886958e2cd3600bd7f
krishal <krishal342@gmail.com>
2026-02-02 20:56:28 +0545

    refactor server.js

--- server.js
+++ server.js
@@ -43,25 +43,25 @@ app.listen(PORT, () => {
 });
 
 
-//Handle unhandled promise rejections (e.g. database connection errors)
-process.on("unhandledRejection", (err) => {
-    console.error(`Unhandled Rejection: ${err.message}`);
-    Server.close(async () => {
-        await disconnectDB();
-        process.exit(1);
-    });
-});
+// //Handle unhandled promise rejections (e.g. database connection errors)
+// process.on("unhandledRejection", (err) => {
+//     console.error(`Unhandled Rejection: ${err.message}`);
+//     Server.close(async () => {
+//         await disconnectDB();
+//         process.exit(1);
+//     });
+// });
 
-// Handle uncaught exceptions
-process.on("uncaughtException", async (err) => {
-    console.error(`Uncaught Exception: ${err.message}`);
-    await disconnectDB();
-    process.exit(1);
-});
+// // Handle uncaught exceptions
+// process.on("uncaughtException", async (err) => {
+//     console.error(`Uncaught Exception: ${err.message}`);
+//     await disconnectDB();
+//     process.exit(1);
+// });
 
-//Greaceful shutdown
-process.on("SIGTERM", async () => {
-    console.log("SIGTERM received. Shutting down gracefully...");
-    await disconnectDB();
-    process.exit(0);
-});
+// //Greaceful shutdown
+// process.on("SIGTERM", async () => {
+//     console.log("SIGTERM received. Shutting down gracefully...");
+//     await disconnectDB();
+//     process.exit(0);
+// });
